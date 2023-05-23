f5523b45355f7e1e47911573738aa35291c0e672
LaviniaAvram <avramlaviniamalina@gmail.com>
2023-05-23 20:43:03 +0300

    client

--- src/pages/health/Health.jsx
+++ src/pages/health/Health.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./health.css";
 import Menu from "../../components/meniu/Menu";
-import Container from "../../components/meniu/Container";
+import TopContainer from "../../components/meniu/TopContainer";
 
 const Health = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className='app'>
       <Menu />
-      <Container />
+      <TopContainer />
+
+      <div className='containerHe'>
+        <h1 className='titlePass'>Password Strength Check</h1>
+        <div className='inputBox'>
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder='password'
+            id='myPass'
+          />
+          <div className='show' onClick={handleTogglePassword}></div>
+        </div>
+        <div className='strengthMeter'></div>
+      </div>
     </div>
   );
 };
+
 export default Health;
